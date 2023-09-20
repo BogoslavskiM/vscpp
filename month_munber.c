70bc33a55f30cae345ed7f8832997cc883154539
BogoslavskiM <90057933+BogoslavskiM@users.noreply.github.com>
2023-09-20 10:45:59 +0300

    program with switch_case => month_number.c

--- month_munber.c
+++ month_munber.c
@@ -9,44 +9,44 @@ int main() {
     scanf("%d", &month_number);
 
     switch (month_number){
-    case 1:
-        printf("January");
-        break;
-    case 2:
-        printf("February");
-        break;
-    case 3:
-        printf("March");
-        break;
-    case 4:
-        printf("April");
-        break;
-    case 5:
-        printf("May");
-        break;
-    case 6:
-        printf("June");
-        break;
-    case 7:
-        printf("July");
-        break;
-    case 8:
-        printf("August");
-        break;
-    case 9:
-        printf("September");
-        break;
-    case 10:
-        printf("October");
-        break;
-    case 11:
-        printf("November");
-        break;
-    case 12:
-        printf("December");
-        break;
-    default:
-        printf("no found month by month number %d", month_number);
+        case 1:
+            printf("January");
+            break;
+        case 2:
+            printf("February");
+            break;
+        case 3:
+            printf("March");
+            break;
+        case 4:
+            printf("April");
+            break;
+        case 5:
+            printf("May");
+            break;
+        case 6:
+            printf("June");
+            break;
+        case 7:
+            printf("July");
+            break;
+        case 8:
+            printf("August");
+            break;
+        case 9:
+            printf("September");
+            break;
+        case 10:
+            printf("October");
+            break;
+        case 11:
+            printf("November");
+            break;
+        case 12:
+            printf("December");
+            break;
+        default:
+            printf("no found month by month number %d", month_number);
     }
     printf("\n");
 
