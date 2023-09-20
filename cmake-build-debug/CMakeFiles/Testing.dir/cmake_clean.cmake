b60ba674710982ed149a533b6dbed970b2d91ca5
BogoslavskiM <90057933+BogoslavskiM@users.noreply.github.com>
2023-09-20 11:44:49 +0300

    cash files

---
+++ cmake-build-debug/CMakeFiles/Testing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Testing.dir/hello_fun.cpp.o"
+  "CMakeFiles/Testing.dir/hello_fun.cpp.o.d"
+  "CMakeFiles/Testing.dir/main.cpp.o"
+  "CMakeFiles/Testing.dir/main.cpp.o.d"
+  "Testing"
+  "Testing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/makar/CLionProjects/Testing
+# Install script for directory: /Users/makar/CLionProjects/vscpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/makar/CLionProjects/Testing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/makar/CLionProjects/vscpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
